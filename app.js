0fc6c65676a3149a5da51eaf59d8d6da1f144f60
Alexandru   Dobra-Somesan <alexdobras@gmail.com>
2017-12-04 17:29:42 +0200

    test-heroku

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const todoController = require('./controllers/todoController')
+
+
+const app = express()
+
+app.listen(process.env.PORT || 4000)
+console.log('This app runs on http://localhost:4000')
+
+app.set('view engine', 'ejs') //set template engine
+app.use(express.static('./public/')) //static files
+
+
+//fire controllers !!!!
+todoController(app)
